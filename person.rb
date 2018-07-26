e860147b99c59d53dc0e951932da23950478b3ca
charmenpinkard <charmenapinkard@gmail.com>
2018-07-26 20:32:36 +0000

    automatically backed up by learn

--- person.rb
+++ person.rb
@@ -1,14 +1,12 @@
 class Person
   
-  def initialize(person, mandi)
+  def initialize(person)
     @person = person  
-    @mandi = mandi 
   end
+  
   def return_person
     @person 
-  end 
-  def return_mandi 
-    @mandi 
-end 
-  puts mandi = Person.new("mandi")
+  end
+
+  mandi = Person.new("mandi")
 end 
